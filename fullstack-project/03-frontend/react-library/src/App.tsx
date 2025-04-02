e3cc354f2bb586261d167298b35b515c407779e5
Austin Martin <alm2016.in@icloud.com>
2025-04-02 17:12:39 -0400

    commit from macOS

--- fullstack-project/03-frontend/react-library/src/App.tsx
+++ fullstack-project/03-frontend/react-library/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import {Navbar} from "./layouts/Navbar/Navbar";
 import {ExploreTopBooks} from "./layouts/homepage/ExploreTopBooks";
+import {Carousel} from "./layouts/homepage/Carousel";
 
 function App() {
   return (
       <div>
         <Navbar/>
         <ExploreTopBooks />
+        <Carousel/>
       </div>
   );
 }
